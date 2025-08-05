04c8378e52b766ad100f870b6c60d4325fca7fe1
deng.xiangyu <desjajja@gmail.com>
2025-08-05 18:27:45 +0800

    init commit

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -25,9 +25,21 @@ enum Command {
 
 mod my_module {
     use super::Command;
-
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        input
+            .iter()
+            .map(|(text, cmd)| match cmd {
+                Command::Uppercase => text.to_uppercase(),
+                Command::Trim => text.trim().to_string(),
+                Command::Append(n) => {
+                    let mut s = text.clone();
+                    s.push_str(&"bar".repeat(*n));
+                    s
+                }
+            })
+            .collect()
+    }
 }
 
 fn main() {
@@ -37,7 +49,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
