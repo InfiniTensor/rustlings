d7c6ad91f094c9fb693f90dd8aff781c9ec2d4b6
GaoJie <xuebibaobei@163.com>
2025-08-06 21:19:15 +0800

    clippy3 pass

--- exercises/22_clippy/clippy3.rs
+++ exercises/22_clippy/clippy3.rs
@@ -18,7 +18,7 @@ fn main() {
     // }
 
     let my_arr = &[
-        -1, -2, -3
+        -1, -2, -3, // 不能没有逗号
         -4, -5, -6
     ];
     println!("My array! Here it is: {my_arr:?}");
@@ -31,7 +31,7 @@ fn main() {
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
-    td::mem::swap(&mut value_a, &mut value_b);
+    std::mem::swap(&mut value_a, &mut value_b);
     // value_a = value_b;
     // value_b = value_a;
     println!("value a: {value_a}; value b: {value_b}");
