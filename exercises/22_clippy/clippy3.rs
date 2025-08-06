9d05cbc3c501a4d0c838e6e7f1ba92edf3f86c57
GaoJie <xuebibaobei@163.com>
2025-08-06 20:59:03 +0800

    clippy3

--- exercises/22_clippy/clippy3.rs
+++ exercises/22_clippy/clippy3.rs
@@ -4,29 +4,26 @@
 #[rustfmt::skip]
 #[allow(unused_variables, unused_assignments)]
 fn main() {
-    // 1. 用 unwrap() 之前没有先确认 Option 是 Some，会 panic。且 if my_option.is_none() 时，调用 unwrap() 是错误的。
-    // Clippy 建议用 match 或 if let 结构。
+    // 1. 安全处理 Option，不再调用 unwrap()
     let my_option: Option<()> = None;
-    if my_option.is_none() {
-        // 这里 unwrap() 会 panic，改成打印 None 即可
-        println!("{:?}", my_option);
+    if let Some(v) = my_option {
+        println!("Got value: {:?}", v);
+    } else {
+        println!("Got None!");
     }
 
-    // 2. 数组定义语法错误，缺少逗号分隔元素
-    let my_arr = &[
-        -1, -2, -3,
-        -4, -5, -6,
-    ];
-    println!("My array! Here it is: {my_arr:?}");
+    // 2. 数组语法已正确，无需变动
+    let my_arr = &[-1, -2, -3, -4, -5, -6];
+    println!("My array! Here it is: {:?}", my_arr);
 
-    // 3. vec![].resize() 返回的是 (), 不能赋值给变量
+    // 3. 清空 Vec，不需要捕获返回值
     let mut my_empty_vec = vec![1, 2, 3, 4, 5];
-    my_empty_vec.clear();
-    println!("This Vec is empty, see? {my_empty_vec:?}");
+    my_empty_vec.clear(); // .resize() 返回 ()，不赋值给变量
+    println!("This Vec is empty, see? {:?}", my_empty_vec);
 
-    // 4. 交换两个变量的值，Clippy 建议用 std::mem::swap
+    // 4. 使用 std::mem::swap
     let mut value_a = 45;
     let mut value_b = 66;
     std::mem::swap(&mut value_a, &mut value_b);
-    println!("value a: {value_a}; value b: {value_b}");
+    println!("value a: {}; value b: {}", value_a, value_b);
 }
