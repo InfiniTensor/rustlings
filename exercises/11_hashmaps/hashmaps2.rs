a6464c63e2d047b806f669ddf2557b2719c609a3
GaoJie <xuebibaobei@163.com>
2025-08-06 19:29:56 +0800

    hashmaps2

--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -33,7 +33,7 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
         if !basket.contains_key(&fruit) {
-            basket.insert(fruit, 1);
+            basket.insert(fruit, 2);
         }
     }
 }
